2b2099daba2223af341eea2ceb3179d3313156d0
SungWook Kim <sungwookkim318@retrica.co>
2024-08-08 10:15:04 +0900

    feat: Xcode project 생성

---
+++ CombineAlbum/CombineAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAlbumApp.swift
+//  CombineAlbum
+//
+//  Created by SungWook Kim on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
